d00f9f7732281366306c2d16e310578e3c72dd29
vitor-hc <vitor_hc@hotmail.com.br>
2021-08-28 00:49:20 -0300

    Introdução ao Angular 8

---
+++ Angular-Criação de lista de cursos/course-manager/src/app/course/course-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from "@angular/core";
+import {Course} from './course';
+@Component({
+
+    selector: 'app-course-list',
+    templateUrl:'./course-list.component.html'
+    
+})
+export class CourseListComponent implements OnInit { 
+
+courses: Course[]=[];
+
+ngOnInit():void{
+    this.courses=[
+        {
+            id:1,
+            name:'Angular: Forms',
+            imageUrl:'/assets/images/forms.png',
+            price:99.99,
+            code:'XPS-8796',
+            duration:120,
+            rating:4.5,
+            releaseDate:'Agost, 27, 2021'
+        },
+
+        {
+            id:2,
+            name:'Angular: HTTP',
+            imageUrl:'/assets/images/http.png',
+            price:89.99,
+            code:'XPS-8797',
+            duration:100,
+            rating:5,
+            releaseDate:'Agost, 29, 2021'
+        }
+    ]
+}
+
+}
